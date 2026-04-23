ae5022464c5729cbd20f7b540ace9d660fcb4036
rameez <rameezwang@gmail.com>
2026-04-23 16:20:19 +0530

    feat: add contact form section and footer component to yoga page

--- src/components/yoga/YogaContact.jsx
+++ src/components/yoga/YogaContact.jsx
@@ -31,7 +31,7 @@ const YogaContact = () => {
                 <svg className="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" /><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" /></svg>
             ),
             title: "Location Address",
-            desc: "Nms Icon, 902, PLOT NO. 194, near Bamandongri Railway Station, Sector 19, Ulwe, Navi Mumbai, Maharashtra 410206"
+            desc: "Nms Icon, 902, Plot No. 194, Near Bamandongri Railway Station, Sector 19, Ulwe, Navi Mumbai, Maharashtra 410206"
         },
         {
             icon: (
